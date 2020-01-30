f890626f90d0076d59ee2e1d35bde87c7ba997b5
raoulantoine <antoine.raoul@live.fr>
2020-01-30 15:43:12 +0100

    first commit

---
+++ src/js/data/buffs.js
@@ -0,0 +1,32 @@
+export let buffs = [
+    {
+        id: 0,
+        bonus: false,
+        infos: 'Can you be quicker?'
+    },
+    {
+        id: 1,
+        bonus: true,
+        infos: 'Take your time this turn.'
+    },
+    {
+        id: 2,
+        bonus: false,
+        infos: 'Can you see something?'
+    },
+    {
+        id: 3,
+        bonus: true,
+        infos: 'Double your score this turn, but also your losses!'
+    },
+    {
+        id: 4,
+        bonus: true,
+        infos: 'You lucky little bastard, take this 5Opts'
+    },
+    {
+        id: 5,
+        bonus: false,
+        infos: 'Sorry mate, you loose 50pts.'
+    }
+];
